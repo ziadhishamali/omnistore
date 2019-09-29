8ddb3505410c2156a00c162b428b112a63265ede
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-09-30 01:15:59 +0200

    initial commit

---
+++ src/components/firebase.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAjStVZY-brqAExj1MPNtxw6SfPJsjBJII",
+    authDomain: "omnistore-ff6df.firebaseapp.com",
+    databaseURL: "https://omnistore-ff6df.firebaseio.com",
+    projectId: "omnistore-ff6df",
+    storageBucket: "",
+    messagingSenderId: "496523278027",
+    appId: "1:496523278027:web:617ab67bf5afe29e1ccd4e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const storage = firebase.storage();
+
+export { db, storage }
