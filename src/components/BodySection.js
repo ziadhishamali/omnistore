e82c242ef5e0ae250e4e878bb9d58831c4957426
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-10-02 21:54:38 +0200

    added paging function for changing pages

--- src/components/BodySection.js
+++ src/components/BodySection.js
@@ -1,47 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ShowCategory from './ShowCategory';
 import ShowWhats from './ShowWhats';
 import SearchBar from './SearchBar';
 import TopSelling from './TopSelling';
 import Paging from './Paging';
 
-const BodySection = ({popularCategories, whatsHot, topSellingItems}) => {
+class BodySection extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 0,
+        }
+    }
 
-    return (
-        <div className="body-section">
-            <div className="left-section">
-                <div className="title title-container-left">
-                    <span>Popular categories</span>
-                </div>
-                <ShowCategory popularCategories={popularCategories} />
+    changeCurrentPage = (currentPage) => {
+        if (currentPage < 0) {
+            currentPage = 0;
+        }
 
-                <div className="title title-container-left">
-                    <span>What's hot</span>
-                </div>
-                <ShowWhats whatsHot={whatsHot} />
+        if (currentPage > 5) {
+            currentPage = 5;
+        }
+        this.setState({currentPage});
+    }
 
-                <div className="title title-container-left">
-                    <span>Promotions</span>
-                </div>
-                <div className="promotions">NO PROMOTIONS A.T.M</div>
-                <div className="promotions-2">NO PROMOTIONS A.T.M</div>
-            </div>
-
-            <div className="right-section">
-                <div className="title">
-                    <div className="title-container-left">
-                        <span>Top selling items</span>
+    render() { 
+        return (
+            <div className="body-section">
+                <div className="left-section">
+                    <div className="title title-container-left">
+                        <span>Popular categories</span>
                     </div>
-                    <div className="title-container-right">
-                        <a href="/#">{"+ view all items"}</a>
+                    <ShowCategory popularCategories={this.props.popularCategories} />
+    
+                    <div className="title title-container-left">
+                        <span>What's hot</span>
                     </div>
+                    <ShowWhats whatsHot={this.props.whatsHot} />
+    
+                    <div className="title title-container-left">
+                        <span>Promotions</span>
+                    </div>
+                    <div className="promotions">NO PROMOTIONS A.T.M</div>
+                    <div className="promotions-2">NO PROMOTIONS A.T.M</div>
                 </div>
-                <SearchBar />
-                <TopSelling topSellingItems={topSellingItems} />
-                <Paging />
+    
+                <div className="right-section">
+                    <div className="title">
+                        <div className="title-container-left">
+                            <span>Top selling items</span>
+                        </div>
+                        <div className="title-container-right">
+                            <a href="/#">{"+ view all items"}</a>
+                        </div>
+                    </div>
+                    <SearchBar />
+                    <TopSelling topSellingItems={this.props.topSellingItems} />
+                    <Paging current={this.state.currentPage} changeCurrentPage={this.changeCurrentPage}/>
+                </div>
             </div>
-        </div>
-    )
+        );
+    }
 }
-
+ 
 export default BodySection;
--- src/components/Paging.js
+++ src/components/Paging.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Paging = () => {
+const Paging = ({current, changeCurrentPage}) => {
 
     let temp = [];
     for (let i = 0; i < 6; i++) {
@@ -8,19 +8,19 @@ const Paging = () => {
     }
     const getButtons = temp.map((item, idx) => {
         let active = "";
-        if (idx === 1) {
+        if (idx === current) {
             active = " active";
         }
         return (
-            <button key={idx} className={"page-button" + active}>{item + 1}</button>
+            <button key={idx} className={"page-button" + active} onClick={() => changeCurrentPage(item)}>{item + 1}</button>
         )
     })
 
     return (
         <div className="paging">
-            <button className="page-prev">{"<"}</button>
+            <button className="page-prev" onClick={() => changeCurrentPage(current - 1)}>{"<"}</button>
             {getButtons}
-            <button className="page-next">{">"}</button>
+            <button className="page-next" onClick={() => changeCurrentPage(current + 1)}>{">"}</button>
         </div>
     )
 }
