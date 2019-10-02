80434c0643b062795fbddf02d60d15bc402151c6
Ziad Hisham Ali <ziadhesham97@gmail.com>
2019-10-02 19:41:31 +0200

    Updates

---
+++ precache-manifest.d24d9e5d87554a647a6dd32d7330efde.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a39a8a608812bcace5e7de9c2b5c32d",
+    "url": "/omnistore/index.html"
+  },
+  {
+    "revision": "56ee2117948df558f317",
+    "url": "/omnistore/static/css/main.9cab99ae.chunk.css"
+  },
+  {
+    "revision": "368032b9c251cbe9ecd8",
+    "url": "/omnistore/static/js/2.80907a07.chunk.js"
+  },
+  {
+    "revision": "56ee2117948df558f317",
+    "url": "/omnistore/static/js/main.f213643e.chunk.js"
+  },
+  {
+    "revision": "5bca28b2ef59cc136d9d",
+    "url": "/omnistore/static/js/runtime-main.d5a1bc5b.js"
+  },
+  {
+    "revision": "348358693ea70c6daa05febfd21a9bd4",
+    "url": "/omnistore/static/media/chat-2.34835869.svg"
+  },
+  {
+    "revision": "9b1cf8aeeb433a6ea68ceb9ec560d5c7",
+    "url": "/omnistore/static/media/facebook.9b1cf8ae.png"
+  },
+  {
+    "revision": "9b613ce3c5e4e883bc5b60e49e508241",
+    "url": "/omnistore/static/media/heart-2.9b613ce3.svg"
+  },
+  {
+    "revision": "386cc387066317cf209c9abf7e98afab",
+    "url": "/omnistore/static/media/lang-uk.386cc387.svg"
+  },
+  {
+    "revision": "d2a6adc663427d202c462a1529dc6b17",
+    "url": "/omnistore/static/media/lock-2.d2a6adc6.svg"
+  },
+  {
+    "revision": "c80ee2d8793e752c267a83a582c8cc9c",
+    "url": "/omnistore/static/media/rss.c80ee2d8.svg"
+  },
+  {
+    "revision": "0e7feb70f12f36a9cdce2e0033fc742a",
+    "url": "/omnistore/static/media/search.0e7feb70.svg"
+  },
+  {
+    "revision": "de41ed2c8be8c8a59dc68809cf92d25e",
+    "url": "/omnistore/static/media/shopping-cart-2.de41ed2c.svg"
+  },
+  {
+    "revision": "d016639703f9aafc18cd4d1631580836",
+    "url": "/omnistore/static/media/star-3.d0166397.svg"
+  },
+  {
+    "revision": "8b6cdc39bce96730807d3271806a323c",
+    "url": "/omnistore/static/media/sunflower.8b6cdc39.svg"
+  },
+  {
+    "revision": "87fdbe4779a09d178853992a03ccbdcc",
+    "url": "/omnistore/static/media/twitter.87fdbe47.svg"
+  }
+]);
